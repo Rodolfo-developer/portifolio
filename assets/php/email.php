7d48c9adda58d5361bdd2a5dc951909111102132
Rodolfo Correa <rodolforj13@gmail.com>
2020-08-19 14:32:29 -0300

    modificando o php

--- assets/php/email.php
+++ assets/php/email.php
@@ -2,18 +2,18 @@
 
 if(isset($_POST['email']) && !empty($_POST['email'])){
 
-$nome = addslashes($_POST['name'])
-$email = addslashes($_POST['email'])
-$mensagem = addslashes($_POST['message'])
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
 
-$to = "rodolforj13@gmail.com";
+$to = "contato@rodolfodeveloper.com";
 $subject = "Contato - Portifolio";
 $body = "Nome: ".$nome. "\r\n".
 		"Email: ".$email. "\r\n".
-		"Mensagem: ".$mensagem
-$header = "From:contato@rodolfoesteves-developer.com"."\r\n"
-			."Reply-To".$email"\r\n"
-			."X=Mailer:PHP/".phpversion();
+		"Mensagem: ".$mensagem;
+$header = "From:contato2@rodolfodeveloper.com"."\r\n".
+			"Reply-To:".$email."\r\n".
+			"X=Mailer:PHP/".phpversion();
 
 if(mail($to,$subject,$body,$header)){
 	echo ("Email enviado com sucesso!");
